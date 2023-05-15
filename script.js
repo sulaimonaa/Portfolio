0c68b81c4a0f2c1c0c8ada32ba3bed89f275a3d5
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-05-15 17:08:18 +0100

    update ES linters

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-let navBtn = document.querySelector('#menuIcon');
-let navMenu = document.querySelector('.navbar');
-let navItem1 = document.querySelector('.nav-item1');
-let navItem2 = document.querySelector('.nav-item2');
-let navItem3 = document.querySelector('.nav-item3');
-let closeMenu = document.querySelector('#closeBtn');
+const navBtn = document.querySelector('#menuIcon');
+const navMenu = document.querySelector('.navbar');
+const navItem1 = document.querySelector('.nav-item1');
+const navItem2 = document.querySelector('.nav-item2');
+const navItem3 = document.querySelector('.nav-item3');
+const closeMenu = document.querySelector('#closeBtn');
 
-navBtn.addEventListener('click', function () {
-    if ( navMenu.className === 'navbar') {
-        navMenu.classList = 'navbar-show'
-        closeMenu.style.display = 'block'
-    } else {
-        navMenu.className = 'navbar'
-    }
-})
+navBtn.addEventListener('click', () => {
+  if (navMenu.className === 'navbar') {
+    navMenu.classList = 'navbar-show';
+    closeMenu.style.display = 'block';
+  } else {
+    navMenu.className = 'navbar';
+  }
+});
 
-closeMenu.addEventListener('click', function(){
-    navMenu.className = 'navbar'
-})
+closeMenu.addEventListener('click', () => {
+  navMenu.className = 'navbar';
+});
 
-navItem1.addEventListener('click', function(){
-    navMenu.className = 'navbar'
-})
+navItem1.addEventListener('click', () => {
+  navMenu.className = 'navbar';
+});
 
-navItem2.addEventListener('click', function(){
-    navMenu.className = 'navbar'
-})
+navItem2.addEventListener('click', () => {
+  navMenu.className = 'navbar';
+});
 
-navItem3.addEventListener('click', function(){
-    navMenu.className = 'navbar'
-})
+navItem3.addEventListener('click', () => {
+  navMenu.className = 'navbar';
+});
