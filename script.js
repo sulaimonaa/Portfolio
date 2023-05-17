39f331b929d6324b748faf162d6a5e9bed7eabc3
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-05-17 13:54:45 +0100

    update config file

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const navBtn = document.querySelector('#menuIcon');
 const navMenu = document.querySelector('.navbar');
-const navItem1 = document.querySelector('.nav-item1');
-const navItem2 = document.querySelector('.nav-item2');
-const navItem3 = document.querySelector('.nav-item3');
+const navItem = document.querySelector('.nav-item');;
 const closeMenu = document.querySelector('#closeBtn');
 
 navBtn.addEventListener('click', () => {
   if (navMenu.className === 'navbar') {
-    navMenu.classList = 'navbar-show';
+    navMenu.className = 'navbar-show';
     closeMenu.style.display = 'block';
   } else {
     navMenu.className = 'navbar';
@@ -18,14 +16,6 @@ closeMenu.addEventListener('click', () => {
   navMenu.className = 'navbar';
 });
 
-navItem1.addEventListener('click', () => {
-  navMenu.className = 'navbar';
-});
-
-navItem2.addEventListener('click', () => {
-  navMenu.className = 'navbar';
-});
-
-navItem3.addEventListener('click', () => {
-  navMenu.className = 'navbar';
+navItem.addEventListener('click', () => {
+  navMenu.classList.toggle('navbar')
 });
