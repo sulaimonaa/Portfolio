637dca2939f03dd5ad1962337ab509de375476f1
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-05-18 21:15:47 +0100

    add contact form validation

--- script.js
+++ script.js
@@ -223,4 +223,22 @@ detailCardClose4.addEventListener('click', () => {
   bodyScroll.classList.remove('no-scroll');
 });
 // Card 4 popup opening closing ends Here
-// Adding Dynamic Data to Works ul ends Here
+
+// form-validation
+const form = document.getElementById('contact');
+form.addEventListener('submit', (event) => {
+  const email = document.getElementById('email').value;
+  const checkString = email.toLowerCase();
+  const errorDiv = document.getElementById('error-div');
+  if (!(checkString === email)) {
+    const errorShow = document.createElement('p');
+    errorShow.textContent = 'Form not submitted, email should be in lowercase.';
+    errorShow.className = 'error-text';
+    errorDiv.appendChild(errorShow);
+    event.preventDefault();
+    return false;
+  }
+
+  errorDiv.remove();
+  return true;
+});
