004132672a88121a4f152d51a8203b877ee43f79
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-05-19 15:12:46 +0100

    preserve data on local storage

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ projects.forEach((project, index) => {
     </div>
     <div class='detail-card-body'>
       <div class='detail-card-body-left'>
-        <p class='pro-details'>${project.projectCompleteDetails}</p>      
+        <p class='paragraph'>${project.projectCompleteDetails}</p>      
       </div>
         <div class='detail-card-body-right'>
             <ul class='detail-card-body-tags'>
@@ -223,6 +223,26 @@ detailCardClose4.addEventListener('click', () => {
   bodyScroll.classList.remove('no-scroll');
 });
 // Card 4 popup opening closing ends Here
+
+// form-validation
+const form = document.getElementById('contact');
+form.addEventListener('submit', (event) => {
+  const email = document.getElementById('email').value;
+  const checkString = email.toLowerCase();
+  const errorDiv = document.getElementById('error-div');
+  if (!(checkString === email)) {
+    const errorShow = document.createElement('p');
+    errorShow.textContent = 'Form not submitted, email should be in lowercase.';
+    errorShow.className = 'error-text';
+    errorDiv.appendChild(errorShow);
+    event.preventDefault();
+    return false;
+  }
+
+  errorDiv.remove();
+  return true;
+});
+
 
 // preserve data
 let contactFormData = JSON.parse(window.localStorage.getItem('contactFormData'));
